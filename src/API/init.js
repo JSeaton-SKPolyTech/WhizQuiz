ad368564b54a7773b8572ba2cbdc96dbfa1d0c6f
J Seaton <seatonj@saskpolytech.ca>
2025-05-09 14:47:36 -0600

    removing env var for dev purposes

--- src/API/init.js
+++ src/API/init.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 console.log(import.meta.env.VITE_SUPABASE_URL);
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
+const supabase = createClient('https://ptkivpyavmxsvoiuppgl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0a2l2cHlhdm14c3ZvaXVwcGdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3Mzc2MDQsImV4cCI6MjA2MjMxMzYwNH0.aKUMUuxIMupV7dI94qaKhi9eKUiNC0htzA18KquQE4U');
 
 let exportTest;
 
