a18c76941d5da6b1503d171a08236135b2613e2e
J Seaton <seatonj@saskpolytech.ca>
2025-05-16 09:16:40 -0600

    added navigation to nav... duh

--- src/components/TeacherNav.jsx
+++ src/components/TeacherNav.jsx
@@ -1,11 +1,26 @@
+import { Navigate, useNavigate } from 'react-router-dom';
+
 import Button from './Button';
 
 const TeacherNav = function(){
+	const navigate = useNavigate();
+	
+	function goToDashboard(){
+		navigate('/dashboard');
+	}
+	function goToNewQuiz(){
+		navigate('/createQuiz');
+	}
+	function startQuiz(){
+
+	}
+
+
 	return (
 		<nav>
-			<Button className='Nav-Button' buttonText="Dashboard" />
-			<Button className='Nav-Button' buttonText="New Quiz" disabled={true} />
-			<Button className='Nav-Button' buttonText="Start Quiz" />
+			<Button className='Nav-Button' buttonText="Dashboard" onClickFunc={goToDashboard} />
+			<Button className='Nav-Button' buttonText="New Quiz" disabled={true} onClickFunc={goToNewQuiz} />
+			<Button className='Nav-Button' buttonText="Start Quiz" onClickFunc={startQuiz} />
 		</nav>
 	)
 };
