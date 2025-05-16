98b6286990eb8c93dbef94eec8043bfd335007bc
J Seaton <seatonj@saskpolytech.ca>
2025-05-16 13:18:25 -0600

    added sign up back end

--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -1,11 +1,37 @@
 import { useState } from "react";
 
+import supabase from '../API/init';
 import loginImage from '../assets/login-image.png';
 import { LabelledInput, Button } from "../components";
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {	
 	const [userEmail, setUserEmail] = useState("");
 	const [userPassword, setUserPassword] = useState("");
+	const [isLogin, setIsLogin] = useState(true);
+	const navigate = useNavigate();
+
+	async function signUp(){
+		const {data, error} = await supabase.auth.signUp({
+			email: userEmail,
+			password: userPassword
+		});
+		if(!error){
+			const response = await supabase.from('user').insert({user_id: data.user.id, role: 'teacher'});
+			if(!response.error){
+				sessionStorage.setItem('loggedIn', true);
+				navigate('/dashboard');
+			}else{
+				console.log(e);
+			}
+		}else{
+			alert(error);
+		}
+	}
+
+	function login(){
+
+	}
 
     return (
 		<div className="login-overlay"> 
@@ -30,7 +56,15 @@ function LoginPage() {
 								state={userPassword}
 							/>
 						</div>
-						<Button buttonText="Login" />
+						{isLogin ? 
+							<>
+								<Button buttonText="Login" onClickFunc={login} /> 
+								<p className="sign-up-text">Don't have an account? <span onClick={()=>{setIsLogin(false)}}>Sign up here!</span></p>
+							</>
+						: 
+							<Button buttonText="Sign Up" onClickFunc={signUp} />
+						}
+						
 					</div>
 				</div>
 			</div>
